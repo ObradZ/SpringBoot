b6e01fc5c87ad519efd8c19f1b5127fa9d3f2b02
ObradovicZ <obradtf@gmail.com>
2018-01-13 17:20:18 +0100

    service is working for now:)

--- mavenproject/src/main/java/com/springboot/dao/StudentDao.java
+++ mavenproject/src/main/java/com/springboot/dao/StudentDao.java
@@ -4,8 +4,9 @@ import com.springboot.entity.Student;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public class StudentDao {
 
     private static Map<Integer,Student> students;
@@ -14,9 +15,9 @@ public class StudentDao {
 	students  = new HashMap<Integer,Student>(){
 	    {
 		put(1,new Student(1,"Mark","Biology"));
-		put(1,new Student(1,"Stive","ComputerScience"));
-		put(1,new Student(1,"Johnny","Math"));
-		put(1,new Student(1,"Nicol","Languges"));
+		put(2,new Student(2,"Stive","ComputerScience"));
+		put(3,new Student(3,"Johnny","Math"));
+		put(4,new Student(4,"Nicol","Languges"));
 	    }
 	};
 	
--- mavenproject/src/main/java/com/springboot/service/StudentService.java
+++ mavenproject/src/main/java/com/springboot/service/StudentService.java
@@ -3,9 +3,13 @@ package com.springboot.service;
 import com.springboot.dao.StudentDao;
 import com.springboot.entity.Student;
 import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@Service
 public class StudentService {
+@Autowired
     private StudentDao studentDao;
 public Collection<Student> returnAllStudents(){
 	return studentDao.returnAllStudents();
